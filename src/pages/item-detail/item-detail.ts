2dd5b8eec5bab1fcc38c513a49e26e5cb1bbd746
danimanrique <daniela.manrique22@hotmail.com>
2017-11-07 12:33:00 -0300

    Lista secciones terminada

--- src/pages/item-detail/item-detail.ts
+++ src/pages/item-detail/item-detail.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Seccion } from '../../models/seccion';
-declare var $: any;
 
 @IonicPage()
 @Component({
@@ -10,14 +9,12 @@ declare var $: any;
 })
 export class ItemDetailPage implements OnInit{
   seccion: any;
-  contenidoX: String;
+  contenido: String;
   constructor(public navCtrl: NavController, navParams: NavParams) {
       this.seccion = navParams.get('seccion');
   }
 
   ngOnInit():void{
-      var $log = $("#contenidoX");
-      var htmlString = $.parseHTML(this.seccion.contenido);
-      $log.append(htmlString);
+    this.contenido = this.seccion.contenido;
   }
 }
