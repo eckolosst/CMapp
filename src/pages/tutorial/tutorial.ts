a9a89b25253332c98b2325ef8f047d2f57234e8f
eckolosst <lucasp.1606@gmail.com>
2017-12-14 13:26:35 -0300

    Despues de generar el apk

--- src/pages/tutorial/tutorial.ts
+++ src/pages/tutorial/tutorial.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, MenuController, NavController, Platform } from 'ionic-angular';
+import { IonicPage, NavController, Platform } from 'ionic-angular';
 import { TranslateService } from '@ngx-translate/core';
-import { MainPage } from '../pages';
+// import { MainPage } from '../pages';
 
 export interface Slide {
   title: string;
@@ -19,7 +19,7 @@ export class TutorialPage {
   showSkip = true;
   dir: string = 'ltr';
 
-  constructor(public navCtrl: NavController, public menu: MenuController, translate: TranslateService, public platform: Platform) {
+  constructor(public navCtrl: NavController, translate: TranslateService, public platform: Platform) {
     this.dir = platform.dir();
     translate.get(["TUTORIAL_SLIDE1_TITLE",
       "TUTORIAL_SLIDE1_DESCRIPTION",
@@ -51,21 +51,11 @@ export class TutorialPage {
   }
 
   startApp() {
-    this.navCtrl.setRoot(MainPage);
+    this.navCtrl.setRoot('MainPage');
   }
 
   onSlideChangeStart(slider) {
     this.showSkip = !slider.isEnd();
-  }
-
-  ionViewDidEnter() {
-    // the root left menu should be disabled on the tutorial page
-    this.menu.enable(false);
-  }
-
-  ionViewWillLeave() {
-    // enable the root left menu when leaving the tutorial page
-    this.menu.enable(true);
   }
 
 }
